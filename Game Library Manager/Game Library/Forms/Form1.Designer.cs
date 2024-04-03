c12e8da1d849d6cba8c5a96ef05cd5d572ac8571
Tuomas Hartikainen <s3hatu00@students.osao.fi>
2024-04-03 14:39:39 +0300

    First commit. Added .gitignore and the project with GUI done

---
+++ Game Library Manager/Game Library/Forms/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿
+namespace Game_Library
+{
+    partial class FormGames
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormGames));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.buttonStrategy = new System.Windows.Forms.Button();
+            this.buttonPuzzle = new System.Windows.Forms.Button();
+            this.buttonArcade = new System.Windows.Forms.Button();
+            this.buttonFilter = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonGames = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelShowGames = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRPS = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonGame2 = new System.Windows.Forms.Button();
+            this.labelGame1 = new System.Windows.Forms.Label();
+            this.buttonGame1 = new System.Windows.Forms.Button();
+            this.panelShowLibrary = new System.Windows.Forms.Panel();
+            this.labelLibraryGame2 = new System.Windows.Forms.Label();
+            this.labelLibraryGame1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panelShowGames.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRPS)).BeginInit();
+            this.panelShowLibrary.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.panel1.Controls.Add(this.buttonStrategy);
+            this.panel1.Controls.Add(this.buttonPuzzle);
+            this.panel1.Controls.Add(this.buttonArcade);
+            this.panel1.Controls.Add(this.buttonFilter);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.buttonGames);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(-2, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(172, 466);
+            this.panel1.TabIndex = 0;
+            // 
+            // buttonStrategy
+            // 
+            this.buttonStrategy.FlatAppearance.BorderSize = 0;
+            this.buttonStrategy.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonStrategy.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonStrategy.ForeColor = System.Drawing.Color.White;
+            this.buttonStrategy.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonStrategy.Location = new System.Drawing.Point(0, 302);
+            this.buttonStrategy.Name = "buttonStrategy";
+            this.buttonStrategy.Size = new System.Drawing.Size(172, 43);
+            this.buttonStrategy.TabIndex = 2;
+            this.buttonStrategy.Text = "Strategy";
+            this.buttonStrategy.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonStrategy.UseVisualStyleBackColor = true;
+            // 
+            // buttonPuzzle
+            // 
+            this.buttonPuzzle.FlatAppearance.BorderSize = 0;
+            this.buttonPuzzle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonPuzzle.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonPuzzle.ForeColor = System.Drawing.Color.White;
+            this.buttonPuzzle.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonPuzzle.Location = new System.Drawing.Point(0, 253);
+            this.buttonPuzzle.Name = "buttonPuzzle";
+            this.buttonPuzzle.Size = new System.Drawing.Size(172, 43);
+            this.buttonPuzzle.TabIndex = 2;
+            this.buttonPuzzle.Text = "Puzzle";
+            this.buttonPuzzle.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonPuzzle.UseVisualStyleBackColor = true;
+            // 
+            // buttonArcade
+            // 
+            this.buttonArcade.FlatAppearance.BorderSize = 0;
+            this.buttonArcade.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonArcade.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonArcade.ForeColor = System.Drawing.Color.White;
+            this.buttonArcade.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonArcade.Location = new System.Drawing.Point(0, 204);
+            this.buttonArcade.Name = "buttonArcade";
+            this.buttonArcade.Size = new System.Drawing.Size(172, 43);
+            this.buttonArcade.TabIndex = 2;
+            this.buttonArcade.Text = "Arcade";
+            this.buttonArcade.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonArcade.UseVisualStyleBackColor = true;
+            // 
+            // buttonFilter
+            // 
+            this.buttonFilter.FlatAppearance.BorderSize = 0;
+            this.buttonFilter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonFilter.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonFilter.ForeColor = System.Drawing.Color.White;
+            this.buttonFilter.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonFilter.Location = new System.Drawing.Point(0, 155);
+            this.buttonFilter.Name = "buttonFilter";
+            this.buttonFilter.Size = new System.Drawing.Size(172, 43);
+            this.buttonFilter.TabIndex = 2;
+            this.buttonFilter.Text = "Filter";
+            this.buttonFilter.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonFilter.UseVisualStyleBackColor = true;
+            this.buttonFilter.Click += new System.EventHandler(this.buttonFilter_Click);
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.Location = new System.Drawing.Point(0, 408);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(172, 43);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "User library";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonGames
+            // 
+            this.buttonGames.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.buttonGames.FlatAppearance.BorderSize = 0;
+            this.buttonGames.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonGames.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.buttonGames.ForeColor = System.Drawing.Color.White;
+            this.buttonGames.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.buttonGames.Location = new System.Drawing.Point(0, 106);
+            this.buttonGames.Name = "buttonGames";
+            this.buttonGames.Size = new System.Drawing.Size(172, 43);
+            this.buttonGames.TabIndex = 2;
+            this.buttonGames.Text = "Games";
+            this.buttonGames.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.buttonGames.UseVisualStyleBackColor = false;
+            this.buttonGames.Click += new System.EventHandler(this.buttonGames_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Teal;
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(172, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 25F);
+            this.label1.Location = new System.Drawing.Point(14, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 45);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "GLM";
+            // 
+            // panelShowGames
+            // 
+            this.panelShowGames.Controls.Add(this.pictureBox1);
+            this.panelShowGames.Controls.Add(this.pictureBoxRPS);
+            this.panelShowGames.Controls.Add(this.label2);
+            this.panelShowGames.Controls.Add(this.buttonGame2);
+            this.panelShowGames.Controls.Add(this.labelGame1);
+            this.panelShowGames.Controls.Add(this.buttonGame1);
+            this.panelShowGames.Location = new System.Drawing.Point(169, -2);
+            this.panelShowGames.Name = "panelShowGames";
+            this.panelShowGames.Size = new System.Drawing.Size(566, 466);
+            this.panelShowGames.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(218, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(175, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBoxRPS
+            // 
+            this.pictureBoxRPS.Image = ((System.Drawing.Image)(resources.GetObject("pictureBoxRPS.Image")));
+            this.pictureBoxRPS.Location = new System.Drawing.Point(16, 14);
+            this.pictureBoxRPS.Name = "pictureBoxRPS";
+            this.pictureBoxRPS.Size = new System.Drawing.Size(175, 175);
+            this.pictureBoxRPS.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxRPS.TabIndex = 2;
+            this.pictureBoxRPS.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label2.Location = new System.Drawing.Point(247, 192);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tic Tac Toe";
+            // 
+            // buttonGame2
+            // 
+            this.buttonGame2.Location = new System.Drawing.Point(252, 224);
+            this.buttonGame2.Name = "buttonGame2";
+            this.buttonGame2.Size = new System.Drawing.Size(102, 23);
+            this.buttonGame2.TabIndex = 0;
+            this.buttonGame2.Text = "Add to the library";
+            this.buttonGame2.UseVisualStyleBackColor = true;
+            this.buttonGame2.Click += new System.EventHandler(this.buttonGame2_Click);
+            // 
+            // labelGame1
+            // 
+            this.labelGame1.AutoSize = true;
+            this.labelGame1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.labelGame1.Location = new System.Drawing.Point(11, 192);
+            this.labelGame1.Name = "labelGame1";
+            this.labelGame1.Size = new System.Drawing.Size(193, 25);
+            this.labelGame1.TabIndex = 1;
+            this.labelGame1.Text = "Rock Paper Scissors";
+            // 
+            // buttonGame1
+            // 
+            this.buttonGame1.Location = new System.Drawing.Point(55, 224);
+            this.buttonGame1.Name = "buttonGame1";
+            this.buttonGame1.Size = new System.Drawing.Size(102, 23);
+            this.buttonGame1.TabIndex = 0;
+            this.buttonGame1.Text = "Add to the library";
+            this.buttonGame1.UseVisualStyleBackColor = true;
+            this.buttonGame1.Click += new System.EventHandler(this.buttonGame1_Click);
+            // 
+            // panelShowLibrary
+            // 
+            this.panelShowLibrary.Controls.Add(this.labelLibraryGame2);
+            this.panelShowLibrary.Controls.Add(this.labelLibraryGame1);
+            this.panelShowLibrary.Controls.Add(this.label3);
+            this.panelShowLibrary.Location = new System.Drawing.Point(169, -2);
+            this.panelShowLibrary.Name = "panelShowLibrary";
+            this.panelShowLibrary.Size = new System.Drawing.Size(566, 466);
+            this.panelShowLibrary.TabIndex = 2;
+            // 
+            // labelLibraryGame2
+            // 
+            this.labelLibraryGame2.AutoSize = true;
+            this.labelLibraryGame2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.labelLibraryGame2.Location = new System.Drawing.Point(11, 182);
+            this.labelLibraryGame2.Name = "labelLibraryGame2";
+            this.labelLibraryGame2.Size = new System.Drawing.Size(64, 25);
+            this.labelLibraryGame2.TabIndex = 1;
+            this.labelLibraryGame2.Text = "label4";
+            // 
+            // labelLibraryGame1
+            // 
+            this.labelLibraryGame1.AutoSize = true;
+            this.labelLibraryGame1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.labelLibraryGame1.Location = new System.Drawing.Point(11, 136);
+            this.labelLibraryGame1.Name = "labelLibraryGame1";
+            this.labelLibraryGame1.Size = new System.Drawing.Size(64, 25);
+            this.labelLibraryGame1.TabIndex = 1;
+            this.labelLibraryGame1.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label3.Location = new System.Drawing.Point(7, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "My games";
+            // 
+            // FormGames
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 461);
+            this.Controls.Add(this.panelShowGames);
+            this.Controls.Add(this.panelShowLibrary);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormGames";
+            this.Text = "Game Library Manager";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panelShowGames.ResumeLayout(false);
+            this.panelShowGames.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRPS)).EndInit();
+            this.panelShowLibrary.ResumeLayout(false);
+            this.panelShowLibrary.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button buttonGames;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonFilter;
+        private System.Windows.Forms.Button buttonStrategy;
+        private System.Windows.Forms.Button buttonPuzzle;
+        private System.Windows.Forms.Button buttonArcade;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panelShowGames;
+        private System.Windows.Forms.Label labelGame1;
+        private System.Windows.Forms.Button buttonGame1;
+        private System.Windows.Forms.Panel panelShowLibrary;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonGame2;
+        private System.Windows.Forms.PictureBox pictureBoxRPS;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelLibraryGame1;
+        private System.Windows.Forms.Label labelLibraryGame2;
+    }
+}
+
---
+++ Game Library Manager/Game Library/Forms/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game_Library
+{
+    public partial class FormGames : Form
+    {
+        public FormGames()
+        {
+            InitializeComponent();
+            buttonArcade.Visible = false;
+            buttonPuzzle.Visible = false;
+            buttonStrategy.Visible = false;
+            panelShowLibrary.Visible = false;
+            panelShowGames.Visible = true;
+        }
+            private void buttonFilter_Click(object sender, EventArgs e)
+        {
+            if (buttonPuzzle.Visible || buttonStrategy.Visible || buttonArcade.Visible)
+            {
+                buttonArcade.Visible = false;
+                buttonPuzzle.Visible = false;
+                buttonStrategy.Visible = false;
+            }
+            else
+            {
+                buttonArcade.Visible = true;
+                buttonPuzzle.Visible = true;
+                buttonStrategy.Visible = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            panelShowLibrary.Visible = true;
+            panelShowGames.Visible = false;
+        }
+
+        private void buttonGames_Click(object sender, EventArgs e)
+        {
+            panelShowLibrary.Visible = false;
+            panelShowGames.Visible = true;
+        }
+
+        private void buttonGame1_Click(object sender, EventArgs e)
+        {
+            AddGameToLibrary();
+        }
+
+        private void buttonGame2_Click(object sender, EventArgs e)
+        {
+            AddGameToLibrary();
+        }
+
+        private void AddGameToLibrary()
+        {
+            MessageBox.Show("Added game to library");
+        }
+    }
+}
